0fd410d95959864d057890c9f330cbcc42d31b5b
fatiht21 <81432838+fatiht21@users.noreply.github.com>
2022-07-30 11:22:05 +0200

    roman-numeral-converter s2

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -60,3 +60,31 @@ function convertToRoman(num) {
 }
 
 console.log(convertToRoman(5));
+
+//s2
+function convertToRoman(num) {
+  const map = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let result = "";
+
+  for (let x in map) {
+   result += x.repeat(Math.floor(num / map[x]));
+   num %= map[x];
+  }
+  return result;
+}
+
+console.log(convertToRoman(6));
